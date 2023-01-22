c62a983191561b2f369bf660b33637a728acb6aa
GokulakrishnanV <62561375+GokulakrishnanV@users.noreply.github.com>
2023-01-22 19:18:05 +0530

    Initial commit & finished Intro

---
+++ src/components/Content.js
@@ -0,0 +1,31 @@
+import introImage from '../assets/images/portfolio-person.png';
+import '../css/Content.css';
+function Introduction() {
+    return (
+        <div className="row" id="intro">
+            <div className="col-md" id="intro-text">
+                <p className="udl-txt" id="intro-welcome">Welcome I'm</p>
+                <h2>Gokulakrishnan V</h2>
+                <h3>Full-stack web and mobile developer</h3>
+                <p className="intro-about">I'm a seasoned full-stack developer</p>
+                <p className="intro-about">I develop applications for Android, iOS, MacOS, Linux, Windows and Web using Flutter, React and PHP</p>
+                <p className="intro-about">Currently I'm working at Nutz Technovation Pvt Ltd., Erode</p>
+                <div className="row row-btn">
+                    <button className="btn-md" id="hire-me-btn">Hire Me</button>
+                    <button className="btn-md" id="portfolio-btn">Portfolio</button>
+                </div>
+            </div>
+            <img src={introImage} alt="Person"></img>
+        </div>
+    )
+}
+
+function Content() {
+    return (
+        <div className="main-content">
+            <Introduction />
+        </div>
+    )
+}
+
+export default Content;
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+function NavBar() {
+    return (
+        <nav className="navbar">
+            <p className="navbar-leading">GK</p>
+            <div className="navbar-actions">
+                <a className="active home" href="#home">Home</a>
+                <a className="about-me" href="#about-me" >About Me</a>
+                <a className="services" href="#services">Services</a>
+                <a className="my-projects" href="#my-projects">My Projects</a>
+                <a className="contact-me" href="#contact">Contact Me</a>
+            </div>
+            {/* <div>
+                <button className="navbar-menu" id="menu-button">
+                    <nav className="navbar-menu-items" id="navbar-menu-items">
+                        <a className="active home" href="#home">Home</a>
+                        <a className="about-me" href="#about-me" >About Me</a>
+                        <a className="services" href="#services">Services</a>
+                        <a className="my-projects" href="#my-projects">My Projects</a>
+                        <a className="contact-me" href="#contact">Contact Me</a>
+                    </nav>
+                </button>
+            </div> */}
+        </nav>
+    );
+}
+
+export default NavBar;
